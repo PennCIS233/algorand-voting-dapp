9f58599990f1c128d7d1e01886093651196a782b
Elijah Jasso <57808638+elijahj19@users.noreply.github.com>
2022-02-08 23:25:52 -0500

    change compile version to 5 if compiled directly from file

--- smart-contract/election_smart_contract.py
+++ smart-contract/election_smart_contract.py
@@ -186,9 +186,9 @@ def clear_state_program():
 
 if __name__ == "__main__":
     with open("vote_approval.teal", "w") as f:
-        compiled = compileTeal(approval_program(), mode=Mode.Application, version=2)
+        compiled = compileTeal(approval_program(), mode=Mode.Application, version=5)
         f.write(compiled)
 
     with open("vote_clear_state.teal", "w") as f:
-        compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=2)
+        compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=5)
         f.write(compiled)
